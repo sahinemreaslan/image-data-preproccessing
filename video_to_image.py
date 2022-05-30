96184670d992b4e563efb868eddfc533479383be
VoidString <43322788+voidstring@users.noreply.github.com>
2022-05-31 02:36:21 +0300

    Update video_to_image.py

--- video_to_image.py
+++ video_to_image.py
@@ -1,10 +1,13 @@
-
 import cv2
-vidcap = cv2.VideoCapture('C:/Users/Emre/Desktop/VID_20211109_022216.mp4')
+vidcap = cv2.VideoCapture('C:/Users/Emre/Desktop/Dataset\TrainDataset/20.mp4')
 success,image = vidcap.read()
-count = 0
+count = 14846
 while success:
-  cv2.imwrite("frame%d.jpg" % count, image,[cv2.IMWRITE_JPEG_QUALITY, 100)     # save frame as JPEG file      
-  success,image = vidcap.read()
-  print('Read a new frame: ', success)
-  count += 1
+    success,image = vidcap.read()
+    print('Read a new frame: ', success)
+    if(count % 3 == 0):
+        cv2.imwrite("frame%d.jpg" % count, image,[cv2.IMWRITE_JPEG_QUALITY, 100])     # save frame as JPEG file      
+    count += 1
+    
+  
+print("Count Value : ",count)   
