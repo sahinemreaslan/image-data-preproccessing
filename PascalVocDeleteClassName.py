e436c606045f8f22f7313d62aaf61c0f66504f7e
VoidString <43322788+voidstring@users.noreply.github.com>
2022-07-02 17:17:20 +0300

    Add files via upload

---
+++ PascalVocDeleteClassName.py
@@ -0,0 +1,17 @@
+# Read in the file
+import glob,os
+dizi = glob.glob("*.xml")
+sayac = 0
+className = '0'
+for i in range(0,len(dizi)):
+  with open(str(dizi[i]), 'r') as file :
+    filedata = file.read()
+    while(filedata.find('<object>\n\t\t<name>0') != -1):
+        print("Stringde var")
+        init_value = filedata.find('<object>\n\t\t<name>'+className)
+        filedata = filedata[0:init_value]+filedata[init_value+220:]
+        print(filedata.find('<object>\n\t\t<name>0'))
+        print(filedata.find('<object>\n\t\t<name>0'))
+  with open(str(dizi[i]), 'w') as file:
+    file.write(filedata)
+    
